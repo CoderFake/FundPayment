7a11b6d9536b8b107e0581280113b202038746fd
coderfake <hoangdieu22022002@gmail.com>
2025-02-24 10:24:30 +0700

    update: add urls

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('accounts/login/', views.UserLoginView.as_view(), name='login'),
-    path('accounts/logout/', views.logout_view, name='logout'),
+    path('login/', views.UserLoginView.as_view(), name='admin_login'),
+    path('logout/', views.logout_view, name='admin_logout'),
+    path('dashboard/', views.IndexView.as_view(), name='dashboard'),
+    path('accounts/register/', views.register, name='register'),
+
+    #api
+    path('api/summary-dashboard/', views.DashboardSummaryAPIView.as_view(), name='summary_dashboard'),
+    path('api/payment-distribution-dashboard/', views.DashboardPaymentDistributionAPIView.as_view(), name='payment_distribution_dashboard'),
+    path('api/payment-dashboard/', views.DashboardPaymentAPIView.as_view(), name='payment_dashboard'),
+    path('api/fund-status-dashboard/', views.DashboardFundStatus.as_view(), name='fund_status_dashboard'),
 ]
