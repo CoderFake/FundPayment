9921e7045fe217e25182236c60379de7c2f51a64
coderfake <hoangdieu22022002@gmail.com>
2025-02-24 10:17:22 +0700

    create: update admin forms

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,7 +1,14 @@
-from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.forms import UsernameField
 from django import forms
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    PasswordChangeForm,
+    UsernameField, 
+    PasswordResetForm,
+    SetPasswordForm
+)
+from django.utils.translation import gettext_lazy as _
+
 
 class LoginForm(AuthenticationForm):
    username = UsernameField(
@@ -13,12 +20,12 @@ class LoginForm(AuthenticationForm):
        )
    )
    password = forms.CharField(
-       label=_("Password"),
+       label=_("Mật khẩu"),
        strip=False,
        widget=forms.PasswordInput(
            attrs={
                "class": "form-control",
-               "placeholder": "Password"
+               "placeholder": "Mật khẩu"
            }
        ),
    )
